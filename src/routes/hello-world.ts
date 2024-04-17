3e01ef9fdf0cdc549e88d463beb16bf69001959d
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-04-17 02:03:57 -0300

    :sparkles: add create user

--- src/routes/hello-world.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-export async function helloWorld(app: FastifyInstance){
-    app.get('/hello', async (request, reply) => {
-        return reply.send("Hello World!")
-    })
-}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
 import fastify from "fastify";
-import { helloWorld } from "./routes/hello-world";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { createUser } from "./routes/create-user";
 
 const app = fastify();
 
-app.register(helloWorld)
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        info: {
+            title: 'Champions Tracker',
+            description: 'Especificações da API para o back-end das aplicações do sistema Champions Tracker',
+            version: '1.0.0'
+        }
+    },
+    transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs'
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createUser)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
     console.log('HTTP Server Running');
