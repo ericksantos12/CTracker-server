3a0fe9204cb085b5deb20e5f6b6ff9f3297784ff
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-04-18 21:35:22 -0300

    🚚 add api route prefix

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import fastify from "fastify";
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { errorHandler } from "./error-handler";
-import { createChampionship } from "./routes/create-championship";
-import { createUser } from "./routes/create-user";
-import { getChampionship } from "./routes/get-championship";
-import dotenv from 'dotenv';
-import { getChampionshipsList } from "./routes/get-championships-list";
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+    jsonSchemaTransform,
+    serializerCompiler,
+    validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { errorHandler } from './error-handler'
+import { createChampionship } from './routes/create-championship'
+import { createUser } from './routes/create-user'
+import { getChampionship } from './routes/get-championship'
+import dotenv from 'dotenv'
+import { getChampionshipsList } from './routes/get-championships-list'
 
 dotenv.config()
 
-const app = fastify();
+const app = fastify()
 
 app.register(fastifySwagger, {
     swagger: {
@@ -19,29 +23,28 @@ app.register(fastifySwagger, {
         produces: ['application/json'],
         info: {
             title: 'Champions Tracker',
-            description: 'Especificações da API para o back-end das aplicações do sistema Champions Tracker',
-            version: '1.0.0'
-        }
+            description:
+                'Especificações da API para o back-end das aplicações do sistema Champions Tracker',
+            version: '1.0.0',
+        },
     },
-    transform: jsonSchemaTransform
+    transform: jsonSchemaTransform,
 })
 
 app.register(fastifySwaggerUi, {
-    routePrefix: '/docs'
+    routePrefix: '/docs',
 })
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(createUser)
-
-app.register(createChampionship)
-app.register(getChampionship)
-app.register(getChampionshipsList)
+app.register(createUser, { prefix: '/api' })
+app.register(createChampionship, { prefix: '/api' })
+app.register(getChampionship, { prefix: '/api' })
+app.register(getChampionshipsList, { prefix: '/api' })
 
 app.setErrorHandler(errorHandler)
 
-app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
-    console.log(`HTTP Server Running`);
-});
-
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+    console.log(`HTTP Server Running`)
+})
