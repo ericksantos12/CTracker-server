e9792323d18ddc80ae6bc0284a3415cd7550a194
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-04-19 23:19:10 -0300

    🎨 change identation

--- src/routes/get-championships-list.ts
+++ src/routes/get-championships-list.ts
@@ -4,82 +4,85 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 
 export async function getChampionshipsList(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/championships',
-    {
-      schema: {
-        summary: 'Get championships list',
-        tags: ['Championship'],
-        querystring: z.object({
-          query: z.string().nullish(),
-          pageIndex: z.string().nullish().default('0').transform(Number),
-        }),
-        response: {
-          200: z.object({
-            championships: z.array(
-              z.object({
-                id: z.number().int(),
-                name: z.string().min(3),
-                picture: z.string().url().nullable(),
-                type: z.enum(['PHYSICAL', 'VIRTUAL']),
-                game: z.string(),
-                createdAt: z.date(),
-                teamsAmount: z.number().int(),
-              })
-            ),
-          }),
-        },
-      },
-    },
-    async (request, reply) => {
-      const { query, pageIndex } = request.query
-
-      const championships = await prisma.championship.findMany({
-        select: {
-          id: true,
-          name: true,
-          picture: true,
-          description: true,
-          status: true,
-          type: true,
-          createdAt: true,
-          game: true,
-          _count: {
-            select: {
-              TeamChampionship: true,
+    app.withTypeProvider<ZodTypeProvider>().get(
+        '/championships',
+        {
+            schema: {
+                summary: 'Get championships list',
+                tags: ['Championship'],
+                querystring: z.object({
+                    query: z.string().nullish(),
+                    pageIndex: z
+                        .string()
+                        .nullish()
+                        .default('0')
+                        .transform(Number),
+                }),
+                response: {
+                    200: z.object({
+                        championships: z.array(
+                            z.object({
+                                id: z.number().int(),
+                                name: z.string().min(3),
+                                picture: z.string().url().nullable(),
+                                type: z.enum(['PHYSICAL', 'VIRTUAL']),
+                                game: z.string(),
+                                createdAt: z.date(),
+                                teamsAmount: z.number().int(),
+                            })
+                        ),
+                    }),
+                },
             },
-          },
         },
-        where: query
-          ? {
-              name: {
-                contains: query,
-              },
-            }
-          : {},
-        skip: pageIndex * 10,
-        take: 10,
-        orderBy: {
-          createdAt: 'desc',
-        },
-      })
+        async (request, reply) => {
+            const { query, pageIndex } = request.query
 
-      console.log(championships);
-      
+            const championships = await prisma.championship.findMany({
+                select: {
+                    id: true,
+                    name: true,
+                    picture: true,
+                    description: true,
+                    status: true,
+                    type: true,
+                    createdAt: true,
+                    game: true,
+                    _count: {
+                        select: {
+                            TeamChampionship: true,
+                        },
+                    },
+                },
+                where: query
+                    ? {
+                          name: {
+                              contains: query,
+                          },
+                      }
+                    : {},
+                skip: pageIndex * 10,
+                take: 10,
+                orderBy: {
+                    createdAt: 'desc',
+                },
+            })
 
-      return reply.send({
-        championships: championships.map((championship) => {
-          return {
-            id: championship.id,
-            name: championship.name,
-            picture: championship.picture,
-            type: championship.type,
-            game: championship.game,
-            createdAt: championship.createdAt,
-            teamsAmount: championship._count.TeamChampionship,
-          }
-        }),
-      })
-    }
-  )
+            console.log(championships)
+
+            return reply.send({
+                championships: championships.map((championship) => {
+                    return {
+                        id: championship.id,
+                        name: championship.name,
+                        picture: championship.picture,
+                        type: championship.type,
+                        game: championship.game,
+                        createdAt: championship.createdAt,
+                        teamsAmount: championship._count.TeamChampionship,
+                    }
+                }),
+            })
+        }
+    )
 }
